2c61544c914992fb9d0d5babd327fbeacfea6405
Александер <sashacharugin@mail.ru>
2022-11-29 12:39:51 +0700

    add support for negative power

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
-double my_pow(double x, unsigned int power) {
+double my_pow(double x, int power) {
+	bool fl = false;
+	if (power < 0) {
+		power = -power;
+		fl = true;
+	}
 	double result = 1;
 	for (int i = 0; i < power; i++)
 		result *= result;
-	return result;
+
+	return fl ? 1 / result : result;
 }
